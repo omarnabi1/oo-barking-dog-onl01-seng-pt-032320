e5540974dd51b799f176d3c3f216ba262138d43b
Omar Nabi <onabi20@gmail.com>
2020-04-07 05:13:48 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,13 +2,15 @@
 
 class Dog 
   def name=(dogs_name)
-    @this_dogs_name = dogs_name
+    @name= dogs_name
   end
   
   def name 
-    @this_dogs_name
+    @name
   end
-  
+end
+
+class Dog   
   def barks 
     puts "woof!"
   end
